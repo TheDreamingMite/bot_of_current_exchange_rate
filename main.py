f49bf906d4a1f83650f22c48c71ca9d75c19f41e
Владимир <vladimir.tikhonov02@mail.ru>
2023-10-19 00:25:32 +0300

    header of bot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
+import asyncio
+import logging # для настройки логгирования, которое поможет в отладке
+
+from aiogram import Bot, Dispatcher
+from aiogram.enums.parse_mode import ParseMode #содержит настройки разметки сообщений (HTML, Markdown)
+from aiogram.fsm.storage.memory import MemoryStorage #хранилища данных для состояний пользователей
+
+import config # настройки бота, пока что только токен
+from handlers import router
 
 
+async def main():
+    bot = Bot(token=config.TOKEN_API, parse_mode=ParseMode.HTML)
+    dp = Dispatcher(storage=MemoryStorage())
+    dp.include_router(router) #подключает к нашему диспетчеру все обработчики
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
